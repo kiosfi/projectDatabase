9dccb623688da03056ab5e655bc001dd01801e62
ssomero <sonja.somero@helsinki.fi>
2015-09-30 15:07:35 +0300

    fixed projects back-end tests

--- packages/custom/projects/server/tests/projects.spec.js
+++ packages/custom/projects/server/tests/projects.spec.js
@@ -10,6 +10,9 @@ var expect = require('expect.js'),
         Project = mongoose.model('Project'),
         Organisation = mongoose.model('Organisation');
 
+
+var project1;
+var project2;
 var project;
 var organisation;
 
@@ -17,6 +20,7 @@ describe('<Unit Test>', function () {
     describe('Model Project:', function () {
         beforeEach(function (done) {
             this.timeout(10000);
+            
             organisation = new Organisation({
                 "name": "Humanrights org",
                 "representative": "Representative",
@@ -24,40 +28,45 @@ describe('<Unit Test>', function () {
                 "tel": "123445",
                 "email": "email@org.com",
                 "website": "www.org.com"});
-            project = new Project({"title": "Human rights",
-                "coordinator": "Keijo Koordinaattori",
-                "organisation": organisation,
-                "status": "approved",
-                "reg_date": "12.10.2014",
-                "funding": {
-                    "applied_curr_local": "50 000",
-                    "applied_curr_eur": "10 000",
-                    "granted_curr_local": "50 000",
-                    "granted_curr_eur": "10 000"},
-                "duration_months": 30,
-                "description": "A short description of project"},
-            {"title": "Humans",
-                "coordinator": "Keijo Koordi",
-                "organisation": organisation,
-                "status": "approved",
-                "reg_date": "12.9.2014",
-                "funding": {
-                    "applied_curr_local": "50 000",
-                    "applied_curr_eur": "11 000",
-                    "granted_curr_local": "50 000",
-                    "granted_curr_eur": "11 000"},
-                "duration_months": 12,
-                "description": "A short description of project"});
-            project.save();
+            project1 = new Project(
+                    {"title": "Human rights",
+                        "coordinator": "Keijo Koordinaattori",
+                        "organisation": organisation,
+                        "status": "approved",
+                        "reg_date": "12.10.2014",
+                        "funding": {
+                            "applied_curr_local": "50 000",
+                            "applied_curr_eur": "10 000",
+                            "granted_curr_local": "50 000",
+                            "granted_curr_eur": "10 000"},
+                        "duration_months": 30,
+                        "description": "A short description of project"});
+            project1.save();
+            project2 = new Project(
+                    {"title": "Humans",
+                        "coordinator": "Keijo Koordi",
+                        "organisation": organisation,
+                        "status": "approved",
+                        "reg_date": "12.9.2014",
+                        "funding": {
+                            "applied_curr_local": "50 000",
+                            "applied_curr_eur": "11 000",
+                            "granted_curr_local": "50 000",
+                            "granted_curr_eur": "11 000"},
+                        "duration_months": 12,
+                        "description": "A short description of project"});
+            project2.save();
             done();
         });
 
         describe('Method All', function () {
 
             it('should list all projects', function (done) {
+
                 this.timeout(10000);
                 var query = Project
                 return query.find(function (err, data) {
+                    console.log(data);
                     expect(err).to.be(null);
                     expect(data.length).to.be(2);
 //                    expect(data[0].title).to.equal("Human rights");
@@ -68,18 +77,23 @@ describe('<Unit Test>', function () {
             });
         });
 
-//        describe('Method Show', function () {
-//
-//            it('should find given project', function (done) {
-//                this.timeout(10000);
-//                var query = Project;
-//                return query.find
-//            });
-//        });
+        describe('Method Show', function () {
+
+            it('should find given project', function (done) {
+                this.timeout(10000);
+                var query = Project;
+                return query.findOne(function (err, data) {
+                    expect(err).to.be(null);
+                    expect(data.title).to.be('Human rights');
+                    done();
+                });
+            });
+        });
 
         afterEach(function (done) {
             this.timeout(10000);
-            project = null;
+            project1.remove();
+            project2.remove();
             done();
         });
     });
