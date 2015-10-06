98894ccd552b4ef8a4ebd90940731f854a7b732a
ssomero <sonja.somero@helsinki.fi>
2015-10-06 18:04:17 +0300

    modifications for spec-files

--- tests/e2e/smoke/projectView.spec.js
+++ tests/e2e/smoke/projectView.spec.js
@@ -8,14 +8,18 @@ describe('Project view page', function () {
         browser.get('/auth/login');
         var email = element(by.model('login.user.email'));
         var password = element(by.model('login.user.password'));
-        email.sendKeys('mr.wisty@gmail.com');
-        password.sendKeys('pe1971tE');
+        email.sendKeys('test@test.com');
+        password.sendKeys('loltest15');
         var submit = element(by.buttonText("Login"));
-        submit.click();
-        browser.get('/');
-        element.all(by.repeater('project in projects'), function(elem) {
-          return elem[0].$('a').click();
+        submit.click().then(function () {
+            
+            element.all(by.repeater('project in projects'), function (elem) {
+                return elem[0].$('a').click();
+                browser.pause();
+            });
         });
+
+
         expect(browser.getCurrentUrl()).toContain('560d27ab7b46bfe2c9bb36c7')
     })
 })
