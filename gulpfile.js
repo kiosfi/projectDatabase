497c85de427e806d739d79f06a643a84ec9ffc11
John Lång <jllang@cs.helsinki.fi>
2015-10-09 15:37:26 +0300

    Added a gulp task 'apidoc' for generating apidoc with doxygen.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var shell = require('gulp-shell');
 
 var env = process.env.NODE_ENV || 'development';
 /*
@@ -15,3 +16,11 @@ gulp.task('default', ['clean'], function (defaultTasks) {
   // run with paramater
   gulp.start(env);
 });
+
+/**
+ * Gulp task for building apidoc.
+ */
+gulp.task('apidoc', shell.task(
+		['cd packages/custom/projects && doxygen']
+	)
+)
