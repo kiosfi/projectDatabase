8cd1848f1c59777a5e5c0b09ae6b3d2fb7cf4871
ssomero <sonja.somero@helsinki.fi>
2015-10-15 12:59:55 +0300

    modified create.html and added categories to form & model

--- packages/custom/projects/server/models/project.js
+++ packages/custom/projects/server/models/project.js
@@ -4,117 +4,140 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema,
-  Organisation = require('./organisation.js'),
-  autoIncrement = require('mongoose-auto-increment');
+        Schema = mongoose.Schema,
+        Organisation = require('./organisation.js'),
+        autoIncrement = require('mongoose-auto-increment');
 
 autoIncrement.initialize(mongoose);
 
 var ProjectSchema = new Schema({
-  project_ref: {
-    type: Number,
-    required: true
-  },
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  coordinator: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  organisation: {
-    type: Schema.ObjectId,
-    ref: 'Organisation',
-    required: true
-  },
-  status: {
-    type: String,
-    required: true
-  },
-  reg_date: {
-    type: Date,
-    default: Date.now
-  },
-  funding: {
-    applied_curr_local: {
-      type: String,
-      required: true,
-      trim: true
+    project_ref: {
+        type: Number,
+        required: true
     },
-    applied_curr_eur: {
-      type: String,
-      required: true,
-      trim: true
+    title: {
+        type: String,
+        required: true,
+        trim: true
     },
-    granted_curr_local: {
-      type: String,
-      required: false,
-      trim: true
+    coordinator: {
+        type: String,
+        required: true,
+        trim: true
     },
-    granted_curr_eur: {
-      type: String,
-      required: false,
-      trim: true
+    organisation: {
+        type: Schema.ObjectId,
+        ref: 'Organisation',
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'rekisteröity',
+        required: true
+    },
+    reg_date: {
+        type: Date,
+        default: Date.now
+    },
+    funding: {
+        applied_curr_local: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        applied_curr_eur: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        granted_curr_local: {
+            type: String,
+            required: false,
+            trim: true
+        },
+        granted_curr_eur: {
+            type: String,
+            required: false,
+            trim: true
+        }
+    },
+    duration_months: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description_en: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    categories: {
+        women: {
+            type: Boolean,
+            default: false
+        },
+        children: {
+            type: Boolean,
+            default: false
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        general: {
+            type: Boolean,
+            default: false
+        },
+        other: {
+            type: Boolean,
+            default: false
+        }
+    },
+    background: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    beneficiaries: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    gender_aspect: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    project_goal: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    sustainability_risks: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    reporting_evaluation: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    other_donors_proposed: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    dac: {
+        type: String,
+        required: true,
+        trim: true
     }
-  },
-  duration_months: {
-    type: Number,
-    required: true,
-    trim: true
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  description_en: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  background: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  beneficiaries: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  gender_aspect: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  project_goal: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  sustainability_risks: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  reporting_evaluation: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  other_donors_proposed: {
-    type: String,
-    required: false,
-    trim: true
-  },
-  dac: {
-    type: String,
-    required: true,
-    trim: true
-  }
 });
 
 ProjectSchema.plugin(autoIncrement.plugin, {
@@ -123,18 +146,18 @@ ProjectSchema.plugin(autoIncrement.plugin, {
     startAt: 15000
 });
 
-ProjectSchema.path('title').validate(function(title) {
-  return !!title;
+ProjectSchema.path('title').validate(function (title) {
+    return !!title;
 }, 'Nimi ei voi olla tyhjä');
 
-ProjectSchema.path('coordinator').validate(function(coordinator) {
-  return !!coordinator;
+ProjectSchema.path('coordinator').validate(function (coordinator) {
+    return !!coordinator;
 }, 'Koordinaattorin nimi ei voi olla tyhjä');
 
-ProjectSchema.statics.load = function(id, cb) {
-  this.findOne({
-    _id: id
-  }).populate({path: 'organisation', model: 'Organisation'}).exec(cb);
+ProjectSchema.statics.load = function (id, cb) {
+    this.findOne({
+        _id: id
+    }).populate({path: 'organisation', model: 'Organisation'}).exec(cb);
 };
 
 mongoose.model('Project', ProjectSchema);
