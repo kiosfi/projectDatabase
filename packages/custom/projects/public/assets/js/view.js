ea5be51b85c638fa9d28bac7edc1640c9cf2ec96
John Lång <jllang@cs.helsinki.fi>
2015-10-15 13:47:47 +0300

    Started working on the collapsing fieldsets feature for project creation form.

--- packages/custom/projects/public/assets/js/view.js
+++ packages/custom/projects/public/assets/js/view.js
@@ -1,5 +1,11 @@
 "use strict";
 
+/**
+ * Hides or shows the given HTML element.
+ *
+ * @param {String} id     id of the element.
+ * @returns {undefined}
+ */
 function toggleCollapse(id) {
     var element = document.getElementById(id);
 
@@ -7,5 +13,25 @@ function toggleCollapse(id) {
         document.getElementById(id).style.display = 'block';
     } else {
         document.getElementById(id).style.display = 'none';
+    }
+}
+
+/**
+ * Hides or shows the given HTML element and changes the animation of the
+ * associated indicator image.
+ *
+ * @param {String} elementID   id of the element.
+ * @param {String} indicatorID id of the indicator.
+ * @return {undefined}
+ */
+function toggleCollapse2(elementID, indicatorID) {
+    var element = document.getElementById(elementID);
+    var indicator = document.getElementById(indicatorID);
+    if (element.style.display !== 'block') {
+        element.style.display = 'block';
+        indicator.src = '../img/indicator-closing.gif';
+    } else {
+        element.style.display = 'none';
+        indicator.src = '../img/indicator-opening.gif';
     }
 }
