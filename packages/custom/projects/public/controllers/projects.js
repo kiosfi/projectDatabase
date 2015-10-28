588d5bf2ad87600a70ad62cf5eedefb6f89dbcb7
Zonsu <johanna.paavola@helsinki.fi>
2015-10-28 15:27:54 +0200

    Show project name in confimation

--- packages/custom/projects/public/controllers/projects.js
+++ packages/custom/projects/public/controllers/projects.js
@@ -7,82 +7,89 @@
  * @param {type} param2
  */
 angular.module('mean.projects').controller('ProjectsController', ['$scope', '$stateParams', '$location', 'Global', 'Projects', 'MeanUser', 'Circles',
-  function($scope, $stateParams, $location, Global, Projects, MeanUser, Circles) {
-    $scope.global = Global;
+    function ($scope, $stateParams, $location, Global, Projects, MeanUser, Circles) {
+        $scope.global = Global;
 
-    $scope.statuses = ['rekisteröity', 'käsittelyssä', 'hyväksytty', 'hylätty',
-                      'sopimus allekirjoitettu', '1. väliraportti', '2. väliraportti',
-                      'loppuraportti', 'päättynyt'];
+        $scope.statuses = ['rekisteröity', 'käsittelyssä', 'hyväksytty', 'hylätty',
+            'sopimus allekirjoitettu', '1. väliraportti', '2. väliraportti',
+            'loppuraportti', 'päättynyt'];
 
-    $scope.coordinators = ['Teppo Tenhunen', 'Kaisa Koordinaattori', 'Maija Maa', 'Juha Jokinen'];
+        $scope.coordinators = ['Teppo Tenhunen', 'Kaisa Koordinaattori', 'Maija Maa', 'Juha Jokinen'];
 
-    $scope.categories = ['naiset', 'lapset', 'vammaiset', 'yleiset ihmisoikeudet', 'muu'];
+        $scope.categories = ['naiset', 'lapset', 'vammaiset', 'yleiset ihmisoikeudet', 'muu'];
 
-    $scope.categorySelection = [];
+        $scope.categorySelection = [];
 
-    $scope.toggleSelection = function toggleSelection(categ) {
-    var idx = $scope.categorySelection.indexOf(categ);
+        $scope.toggleSelection = function toggleSelection(categ) {
+            var idx = $scope.categorySelection.indexOf(categ);
 
-    // is currently selected
-    if (idx > -1) {
-      $scope.categorySelection.splice(idx, 1);
-    }
-
-    // is newly selected
-    else {
-      $scope.categorySelection.push(categ);
-    }
-  };
-
-    $scope.hasAuthorization = function(project) {
-      if (!project) return false;
-      return MeanUser.isAdmin;
-    };
-
-    $scope.create = function(isValid) {
-      if (isValid) {
-        var project = new Projects($scope.project);
-        project.categories = $scope.categorySelection;
-        project.$save(function(response) {
-          $location.path('projects/' + response._id);
-        });
-
-        $scope.project = {};
-
-      } else {
-        $scope.submitted = true;
-      }
-    };
-
-    $scope.find = function() {
-      Projects.query(function(projects) {
-        $scope.projects = projects;
-      });
-    };
-
-    $scope.findOne = function() {
-      Projects.get({
-        projectId: $stateParams.projectId
-      }, function(project) {
-        $scope.project = project;
-      });
-    };
-
-    $scope.remove = function(project) {
-      if (project) {
-        project.$remove(function(response) {
-          for (var i in $scope.projects) {
-            if ($scope.projects[i] === project) {
-              $scope.projects.splice(i, 1);
+            // is currently selected
+            if (idx > -1) {
+                $scope.categorySelection.splice(idx, 1);
             }
-          }
-          $location.path('projects');
-        });
-      } else {
-        $scope.project.$remove(function(response) {
-          $location.path('projects');
-        });
-      }
-    };
-  }
+
+            // is newly selected
+            else {
+                $scope.categorySelection.push(categ);
+            }
+        };
+
+        $scope.hasAuthorization = function (project) {
+            if (!project)
+                return false;
+            return MeanUser.isAdmin;
+        };
+
+        $scope.create = function (isValid) {
+            if (isValid) {
+                var project = new Projects($scope.project);
+                project.categories = $scope.categorySelection;
+                project.$save(function (response) {
+                    $location.path('projects/' + response._id);
+                });
+
+                $scope.project = {};
+
+            } else {
+                $scope.submitted = true;
+            }
+        };
+
+        $scope.find = function () {
+            Projects.query(function (projects) {
+                $scope.projects = projects;
+            });
+        };
+
+        $scope.findOne = function () {
+            Projects.get({
+                projectId: $stateParams.projectId
+            }, function (project) {
+                $scope.project = project;
+            });
+        };
+        
+        $scope.confirm = function(project) {
+    if (confirm("Haluatko varmasti poistaa hankkeen '" + project.title + "'?")) {
+        $scope.remove(project);
+    }
+};
+
+        $scope.remove = function (project) {
+            if (project) {
+                project.$remove(function (response) {
+                    for (var i in $scope.projects) {
+                        if ($scope.projects[i] === project) {
+                            $scope.projects.splice(i, 1);
+                        }
+                    }
+                    $location.path('projects');
+                });
+            } else {
+                $scope.project.$remove(function (response) {
+                    $location.path('projects');
+                });
+            }
+        };
+    }
 ]);
