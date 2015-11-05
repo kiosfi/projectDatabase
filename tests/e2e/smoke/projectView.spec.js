a51413663cf9f8258abcd63cae3fad463eef6054
Maria Niku <maria.niku@cs.helsinki.fi>
2015-11-05 13:08:55 +0200

    testwork continues

--- tests/e2e/smoke/projectView.spec.js
+++ tests/e2e/smoke/projectView.spec.js
@@ -5,7 +5,7 @@ describe('Project view page', function () {
     it('should stay project view page if logged in ', function () {
         helpers.login();
         element.all(by.repeater('project in projects')).
-                get(0).element(by.id('projname')).click(); 
+                get(0).element(by.id('projname')).$('a').click();
         expect(browser.getCurrentUrl()).toContain('56091cbc00fccd6d66bc5cc3');
         helpers.logout();
     });
@@ -13,7 +13,7 @@ describe('Project view page', function () {
     it('should show details of project if logged in', function () {
         helpers.login();
         element.all(by.repeater('project in projects')).
-                get(0).$('a').click();
+                get(0).element(by.id('projname')).$('a').click(); 
 
         var header = element(by.css('section')).element(by.tagName('h2'));
         expect(header.getText()).toContain('Human rights');
