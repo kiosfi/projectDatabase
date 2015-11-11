3696f48a6b94bad2c43228ad476eb490c79d805e
ssomero <sonja.somero@helsinki.fi>
2015-11-11 13:03:00 +0200

    working e2e test for approved-state and some fixes to other e2e tests

--- tests/e2e/smoke/projectList.spec.js
+++ tests/e2e/smoke/projectList.spec.js
@@ -4,15 +4,15 @@ describe('Project list page', function() {
         // change below line to browser.get('/projects') once logging in implemented
         // in e2e testing
         browser.get('/');
-        expect(element.all(by.repeater('project in projects')).count()).toEqual(3);
+        expect(element.all(by.repeater('project in projects')).count()).toEqual(7);
 
         expect(element(by.id('proj-70001')).isPresent()).toBe(true);
         expect(element(by.id('proj-70002')).isPresent()).toBe(true);
         expect(element(by.id('proj-70003')).isPresent()).toBe(true);
-        expect(element(by.id('proj-70004')).isPresent()).toBe(false);
-    }),
-
-    xit('should list registered projects in correct order', function() {
-
+        expect(element(by.id('proj-79999')).isPresent()).toBe(false);
     })
-})
+
+//    it('should list registered projects in correct order', function() {
+//
+//    })
+});
