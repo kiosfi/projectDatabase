d720ababea228510373c048e82f08315c5564990
ssomero <sonja.somero@helsinki.fi>
2015-12-03 11:57:43 +0200

    added getCsvData to include more data

--- packages/custom/search/public/controllers/search.js
+++ packages/custom/search/public/controllers/search.js
@@ -44,17 +44,22 @@ angular.module('mean.search').controller('SearchController', ['$scope', '$stateP
         };
 
         $scope.getCsvData = function () {
+
             $scope.parsedData = [];
-            angular.forEach($scope.results, function(obj) {
+            angular.forEach($scope.results, function (obj) {                
+                
                 $scope.parsedData.push({project_ref: obj.project_ref, title: obj.title, coordinator: obj.coordinator,
-                organisation: obj.organisation.name, description: obj.description, description_en: obj.description_en,
-            duration_months: obj.duration_months,  background: obj.background, gender_aspect: obj.gender_aspect, beneficiaries: obj.beneficiaries,
-        project_goal: obj.project_goal, reporting_evalation: obj.reporting_evaluation, sustainability_risks: obj.sustainability_risks,
-    other_donors_proposed: obj.other_donors_proposed, region: obj.region, dac: obj.dac, in_review_date: obj.in_review.date, 
-    in_review_comments: obj.in_review.comments});
+                    organisation: obj.organisation.name, description: obj.description, description_en: obj.description_en,
+                    duration_months: obj.duration_months, applied_sum_eur: obj.funding.applied_curr_eur, granted_sum: obj.approved.granted_sum_eur, 
+                    left_eur: obj.funding.left_eur, background: obj.background, gender_aspect: obj.gender_aspect, beneficiaries: obj.beneficiaries,
+                    project_goal: obj.project_goal, reporting_evalation: obj.reporting_evaluation, sustainability_risks: obj.sustainability_risks,
+                    other_donors_proposed: obj.other_donors_proposed, region: obj.region, dac: obj.dac, in_review_date: obj.in_review.date,
+                    in_review_comments: obj.in_review.comments, approved_date: obj.approved.approved_date, approved_by: obj.approved.approved_by,
+                    approved_methods:obj.approved_methods, approved_themes: obj.approved_themes,
+                signed_date: obj.signed.signed_date});
 //ended: obj.ended.date
             });
-            
+
         };
 
 
