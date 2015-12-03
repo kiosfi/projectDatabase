54eaa1ba9dde1cca97265238259309c02c3ec2e5
ssomero <sonja.somero@helsinki.fi>
2015-12-03 11:39:43 +0200

    added function to parse nested data so that it is readable in csv file

--- packages/custom/search/public/controllers/search.js
+++ packages/custom/search/public/controllers/search.js
@@ -2,13 +2,12 @@
 
 /* jshint -W098 */
 angular.module('mean.search').controller('SearchController', ['$scope', '$stateParams',
-
     '$http', '$window', 'Global', 'Search', 'OrgSearch', 'ThemeSearch', 'MeanUser',
     function ($scope, $stateParams, $http, $window, Global, Search, OrgSearch, ThemeSearch, MeanUser) {
         $scope.global = Global;
 
         $scope.fields = [{"name": "state", "fi": "Tila"}, {"name": "region", "fi": "Alue"},
-        {"name": "approved.themes", "fi": "Teemat"}]
+            {"name": "approved.themes", "fi": "Teemat"}]
 
 
         $scope.themes = ['Oikeusvaltio ja demokratia', 'TSS-oikeudet', 'Oikeus koskemattomuuteen ja inhimilliseen kohteluun',
@@ -28,20 +27,35 @@ angular.module('mean.search').controller('SearchController', ['$scope', '$stateP
         $scope.search = function () {
             Search.searchProjects($scope.addedQueries, function (searchresults) {
                 $scope.results = searchresults;
+                $scope.getCsvData();
             });
         };
 
         /**
-        * Creates query object from each $scope.field/
-        * $scope.value pair
-        */
+         * Creates query object from each $scope.field/
+         * $scope.value pair
+         */
         $scope.addQuery = function () {
-          $scope.addedQueries.push({});
+            $scope.addedQueries.push({});
         };
 
         $scope.removeQuery = function () {
-          $scope.addedQueries.splice(-1, 1);
-        }
+            $scope.addedQueries.splice(-1, 1);
+        };
+
+        $scope.getCsvData = function () {
+            $scope.parsedData = [];
+            angular.forEach($scope.results, function(obj) {
+                $scope.parsedData.push({project_ref: obj.project_ref, title: obj.title, coordinator: obj.coordinator,
+                organisation: obj.organisation.name, description: obj.description, description_en: obj.description_en,
+            duration_months: obj.duration_months,  background: obj.background, gender_aspect: obj.gender_aspect, beneficiaries: obj.beneficiaries,
+        project_goal: obj.project_goal, reporting_evalation: obj.reporting_evaluation, sustainability_risks: obj.sustainability_risks,
+    other_donors_proposed: obj.other_donors_proposed, region: obj.region, dac: obj.dac, in_review_date: obj.in_review.date, 
+    in_review_comments: obj.in_review.comments});
+//ended: obj.ended.date
+            });
+            
+        };
 
 
     }
