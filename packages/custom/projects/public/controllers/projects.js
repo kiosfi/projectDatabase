fc184e0b48e6b13de81dcb7384c96b0c4958f6d4
John Lång <jllang@cs.helsinki.fi>
2016-03-28 18:24:20 +0300

    Removed a leftover piece of code from my ngUpload experiment.

--- packages/custom/projects/public/controllers/projects.js
+++ packages/custom/projects/public/controllers/projects.js
@@ -609,20 +609,6 @@ angular.module('mean.projects').controller('ProjectsController', ['$scope', '$st
 ////            $scope.project.$accessAppendix(url + "&action=download");
 //        }
 
-        $scope.uploadComplete = function (content) {
-            $scope.response = JSON.parse(content); // Presumed content is a json string!
-            $scope.response.style = {
-                color: $scope.response.color,
-                "font-weight": "bold"
-            };
-
-            // Clear form (reason for using the 'ng-model' directive on the input elements)
-            $scope.fullname = '';
-            $scope.gender = '';
-            $scope.color = '';
-            // Look for way to clear the input[type=file] element
-        };
-
         /**
          * Updates project with "int report" state data
          *
