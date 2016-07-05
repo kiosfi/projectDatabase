8eaab9955a75c06ce31888c1bfb03b3d9e883b52
John Lång <jllang@cs.helsinki.fi>
2016-07-05 11:07:50 +0300

    Improving the server side tests for organisations.

--- packages/custom/organisations/server/tests/organisations.spec.js
+++ packages/custom/organisations/server/tests/organisations.spec.js
@@ -11,26 +11,25 @@ var expect = require('expect.js'),
         BankAccount = mongoose.model('BankAccount');
 var async = require('async');
 
-var organisation;
-var organisation2;
-var organisation3;
-var organisation4;
-var bank_account;
-var bank_account2;
-
 describe('<Unit Test>', function () {
-    describe('Model Organisation:', function () {
 
+    var organisation1;
+    var organisation2;
+    var organisation3;
+    var organisation4;
+    var bank_account1;
+    var bank_account2;
+
+    describe('Model Organisation:', function () {
         beforeEach(function (done) {
             this.timeout(10000);
-
-            bank_account = new BankAccount({
+            bank_account1 = new BankAccount({
                 "bank_contact_details": "Branch, address",
                 "iban": "abcdefg1234",
                 "swift": "OKOYFI",
                 "holder_name": "John Smith"});
-            bank_account.save();
-            organisation = new Organisation({
+            bank_account1.save();
+            organisation1 = new Organisation({
                 "schema_version": 3,
                 "name": "Human rights org",
                 "representative": {
@@ -49,16 +48,16 @@ describe('<Unit Test>', function () {
                 "tel": "123445",
                 "email": "email@org.com",
                 "website": "www.org.com",
-                "legal_status": "legal status",
-                "history_status": "history status",
-                "int_links": "international links",
-                "nat_local_links": "local human rights org",
+                "legal_status": "legal status1",
+                "history_status": "history status1",
+                "int_links": "international links1",
+                "nat_local_links": "local human rights org1",
                 "description": "description for organisation .....",
-                "other_funding_budget": "other funders",
+                "other_funding_budget": "other funders1",
                 "accounting_audit": "accounting",
-                "bank_account": bank_account,
+                "bank_account": bank_account1,
                 "background": "Nulla pretium feugiat dolor, quis tempor enim dignissim at."});
-            organisation.save();
+            organisation1.save();
             organisation2 = new Organisation({
                 "schema_version": 3,
                 "name": "Children's Rights Org",
@@ -78,14 +77,14 @@ describe('<Unit Test>', function () {
                 "tel": "1234456",
                 "email": "email@cro2.com",
                 "website": "www.cro2.com",
-                "legal_status": "legal statuses",
-                "history_status": "history statuses",
-                "int_links": "international link",
+                "legal_status": "legal statuses2",
+                "history_status": "history statuses2",
+                "int_links": "international link2",
                 "nat_local_links": "local human rights org 2",
                 "description": "description for organisation .....",
-                "other_funding_budget": "other funders",
+                "other_funding_budget": "other funders2",
                 "accounting_audit": "audit",
-                "bank_account": bank_account,
+                "bank_account": bank_account1,
                 "background": "Sed tempus lacus a libero finibus, id blandit odio maximus."});
             organisation2.save();
             organisation3 = new Organisation({
@@ -107,27 +106,23 @@ describe('<Unit Test>', function () {
                 "tel": "5551234",
                 "email": "contact@wrng.org",
                 "website": "www.wrng.org",
-                "legal_status": "legal statuses",
-                "history_status": "history statuses",
-                "int_links": "international link",
+                "legal_status": "legal statuses3",
+                "history_status": "history statuses3",
+                "int_links": "international link3",
                 "nat_local_links": "local human rights org 3",
                 "description": "description for organisation .....",
-                "other_funding_budget": "other funders",
-                "accounting_audit": "audit",
-                "bank_account": bank_account,
+                "other_funding_budget": "other funders3",
+                "accounting_audit": "audit2",
+                "bank_account": bank_account1,
                 "background": "Quisque eget nisi a ex porttitor ultricies quis eget sem."});
             organisation3.save();
             done();
         });
 
         describe('Method All', function () {
-
             it('should list all organisations', function (done) {
-
                 this.timeout(10000);
-
                 var query = Organisation.find();
-
                 return query.exec(function (err, orgs) {
                     expect(err).to.be(null);
                     expect(orgs.length).to.equal(3);
@@ -137,18 +132,16 @@ describe('<Unit Test>', function () {
         });
 
         describe('Method Save', function () {
-
             beforeEach(function (done) {
                 this.timeout(10000);
-
                 bank_account2 = new BankAccount({
                     "bank_contact_details": "Bank Branch, address",
                     "iban": "EU11111113333334",
                     "swift": "NDEAFIHH",
-                    "holder_name": "Jack Jackson"});
-
+                    "holder_name": "Jack Jackson"
+                });
                 organisation4 = new Organisation({
-                    "name": "Children rights org",
+                    "name": "Disabled people's rights org",
                     "representative": {
                         "name": "Mr Jackson",
                         "email": "email@email.com",
@@ -163,71 +156,71 @@ describe('<Unit Test>', function () {
                         "country": "Countryland"
                     },
                     "tel": "+111123445",
-                    "email": "email@childrenorg.com",
-                    "website": "www.childrenorg.com",
+                    "email": "email@disabledpeoplesorg.com",
+                    "website": "www.disabledpeoplesorg.com",
                     "legal_status": "non-profit",
-                    "history_status": "history status",
-                    "int_links": "international links",
-                    "nat_local_links": "local human rights org 2",
-                    "description": "description for organisation .....",
-                    "other_funding_budget": "other funders",
+                    "history_status": "history status4",
+                    "int_links": "international links4",
+                    "nat_local_links": "local human rights org 4",
+                    "description": "blah blah",
+                    "other_funding_budget": "other funders4",
                     "accounting_audit": "Auditing",
-                    "bank_account": bank_account2});
-
+                    "bank_account": bank_account2
+                });
                 done();
             });
 
             it('should be able to save organisation without problems', function (done) {
-
                 this.timeout(10000);
-
                 return organisation4.save(function (err, data) {
                     expect(err).to.be(null);
-                    expect(data.name).to.equal('Children rights org');
+                    expect(data.name).to.equal("Disabled people's rights org");
                     expect(data.bank_account.swift).to.equal('NDEAFIHH');
-                    organisation4.remove();
-                    bank_account2.remove();
-                    done();
                 });
             });
 
             it('should show an error when try to save without a name', function (done) {
                 this.timeout(10000);
-
-                organisation4.name = null;
-
+//                organisation4.name = null;
+                organisation4.name = undefined;
                 return organisation4.save(function (err) {
                     expect(err).to.not.be(null);
-                    organisation4.remove();
-                    bank_account2.remove();
-                    done();
                 });
+            });
+
+            afterEach(function (done) {
+                organisation4.remove();
+                bank_account2.remove();
+                done();
             });
         });
 
         describe('Method Show', function () {
-
             it('should find given organisation', function (done) {
                 this.timeout(10000);
-                var query = Organisation
-                return query.findOne({name: "Children's Rights Org"}).exec(function (err, data) {
-                    expect(err).to.be(null);
-                    expect(data.name).to.be("Children's Rights Org");
-                    done();
-                });
+                return Organisation.findOne({name: "Children's Rights Org"})
+                        .exec(function (err, data) {
+                            expect(err).to.be(null);
+                            expect(data.name).to.be("Children's Rights Org");
+                            done();
+                        });
             });
         });
 
         describe('Method Destroy', function () {
             it('should delete given organisation', function (done) {
                 this.timeout(10000);
-                var query = Organisation;
-                return query.remove({name: "Human rights org"}).exec(function (err) {
-                    expect(err).to.be(null);
-                    done();
-                });
+                return Organisation.remove({name: "Human rights org"})
+                        .exec(function (err) {
+                            expect(err).to.be(null);
+                            return Organisation.findOne({name: "Human rights org"})
+                                    .exec(function (err2, data) {
+                                expect(err2).to.be(null);
+                                expect(data).to.be(null);
+                                done();
+                            })
+                        });
             });
-
         });
 
         describe('Method Update', function () {
@@ -248,10 +241,10 @@ describe('<Unit Test>', function () {
 
         afterEach(function (done) {
             this.timeout(10000);
-            organisation.remove();
+            organisation1.remove();
             organisation2.remove();
             organisation3.remove();
-            bank_account.remove();
+            bank_account1.remove();
             done();
         });
     });
